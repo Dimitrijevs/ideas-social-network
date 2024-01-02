d9fa13292c738f157265eca0994fe5fd34aef743
Dimitrijevs <githubodinlv@gmail.com>
2024-01-02 19:28:37 +0200

    feed page

--- resources/views/ideas/shared/idea-card.blade.php
+++ resources/views/ideas/shared/idea-card.blade.php
@@ -50,9 +50,9 @@
             @include('ideas.shared.like-button')
             <div>
                 <span class="fs-6 fw-light text-muted"> <span class="fas fa-clock"> </span>
-                    {{ $idea->created_at }} </span>
+                    {{ $idea->created_at->diffForHumans() }} </span>
             </div>
         </div>
-        @include('shared.commnets-box')
+        @include('ideas.shared.commnets-box')
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FeedController;
 use App\Http\Controllers\IdeaController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\CommentController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\IdeaLikeController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -57,6 +58,8 @@ Route::post('ideas/{idea}/like', [IdeaLikeController::class, 'like'])->middlewar
 Route::post('ideas/{idea}/unlike', [IdeaLikeController::class, 'unlike'])->middleware('auth')->name('ideas.unlike');
 
 Route::get('/profile', [UserController::class, 'profile'])->middleware('auth')->name('profile');
+
+Route::get('/feed', FeedController::class)->middleware('auth')->name('feed');
 
 
 Route::get('/terms', function() {
